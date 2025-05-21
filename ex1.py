f69c39d19c4ec0cb9cf16c64c8aaa0052551305a
dedeza25 <andrezanascimento@colegiopaulodetarso.com.br>
2025-05-21 16:11:02 +0000

    1

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# Solicita um número inteiro positivo ao usuário
+X = int(input("Digite um número: "))
+
+# Utiliza o laço while para imprimir os números de X até 0
+while X >= 0:
+    print(X)
+    X -= 1
+
